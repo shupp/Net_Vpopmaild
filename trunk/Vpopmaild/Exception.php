8aa961d727bfe447a1a52c963a169e514c03005f
shupp <shupp@e28445f6-1c35-0410-a241-e50f2cb363b8>
2007-07-15 23:28:17 +0000

    added Net_Vpopmaild_Exception class
    git-svn-id: file:///Users/bill/svn/mboxsvn/Net_Vpopmaild@4 e28445f6-1c35-0410-a241-e50f2cb363b8

---
+++ trunk/Vpopmaild/Exception.php
@@ -0,0 +1,48 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Net_Vpopmaild_Exception 
+ * 
+ * @uses        PEAR_Exception
+ * @package     Net_Vpopmaild
+ * @author      Joe Stump <joe@joestump.net>
+ */
+
+require_once 'PEAR/Exception.php';
+
+/**
+ * Net_Vpopmaild_Exception
+ *
+ * A small layer above PEAR_Exception that allows you to pass PEAR_Error
+ * as the message to your exceptions.
+ *
+ * @author      Joe Stump <joe@joestump.net>
+ * @package     Net_Vpopmaild
+ */
+class Net_Vpopmaild_Exception extends PEAR_Exception
+{
+    /**
+     * __construct
+     *
+     * @access      public
+     * @param       mixed       $message        PEAR_Error or your message
+     * @param       int         $code           An error code
+     * @return      void
+     */
+    public function __construct($message, $code = 0)
+    {
+        if (PEAR::isError($message)) {
+            $msg = $message->getMessage();
+            $code = $message->getCode();
+        } else {
+            $msg = $message;
+        }
+
+        $this->message  = $msg;
+        $this->code     = $code;
+    }
+}
+
+?>
