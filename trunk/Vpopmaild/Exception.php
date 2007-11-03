03b4ddcca36c3a61d0d61f4759f931bb07f404bd
shupp <shupp@e28445f6-1c35-0410-a241-e50f2cb363b8>
2007-11-03 17:52:18 +0000

    added category tags for exception classes
    git-svn-id: file:///Users/bill/svn/mboxsvn/Net_Vpopmaild@99 e28445f6-1c35-0410-a241-e50f2cb363b8

--- trunk/Vpopmaild/Exception.php
+++ trunk/Vpopmaild/Exception.php
@@ -7,6 +7,7 @@
  * 
  * @uses        PEAR_Exception
  * @package     Net_Vpopmaild
+ * @category    Net
  */
 
 /**
--- trunk/Vpopmaild/FatalException.php
+++ trunk/Vpopmaild/FatalException.php
@@ -7,6 +7,7 @@
  * 
  * @uses        PEAR_Exception
  * @package     Net_Vpopmaild
+ * @category    Net
  * @author      Joe Stump <joe@joestump.net>
  */
 
