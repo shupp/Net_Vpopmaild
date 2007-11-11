80e5911a84ecd6f9190336da712d7e2a2497e64c
shupp <shupp@e28445f6-1c35-0410-a241-e50f2cb363b8>
2007-11-11 01:43:08 +0000

    CS cleanups in Vpopmaild_Exception
    git-svn-id: file:///Users/bill/svn/mboxsvn/Net_Vpopmaild@102 e28445f6-1c35-0410-a241-e50f2cb363b8

--- trunk/Vpopmaild/Exception.php
+++ trunk/Vpopmaild/Exception.php
@@ -5,33 +5,40 @@
 /**
  * Net_Vpopmaild_Exception 
  * 
- * @uses        PEAR_Exception
- * @package     Net_Vpopmaild
- * @category    Net
+ * PHP Version 5
+ * 
+ * @uses     PEAR_Exception
+ * @category Net
+ * @package  Net_Vpopmaild
+ * @author   Bill Shupp <hostmaster@shupp.org>
+ * @license  PHP 3.01  {@link http://www.php.net/license/3_01.txt}
+ * @link     http://shupp.org/Net_Vpopmaild NetVpopmaild
  */
 
-/**
- *  require_once 'PEAR/Exception.php';
- */
 require_once 'PEAR/Exception.php';
 
 /**
  * Net_Vpopmaild_Exception
  *
  * A small layer above 
- * {@link http://pear.php.net/manual/en/core.pear.pear-exception.php PEAR_Exception} with which you can send vpopmaild error message strings
+ * {@link http://pear.php.net/manual/en/core.pear.pear-exception.php PEAR_Exception} 
+ *  with which you can send vpopmaild error message strings
  *
- * @author      Bill Shupp <hostmaster@shupp.org>
- * @package     Net_Vpopmaild
+ * @category Net
+ * @package  Net_Vpopmaild
+ * @author   Bill Shupp <hostmaster@shupp.org>
+ * @license  PHP 3.01  {@link http://www.php.net/license/3_01.txt}
+ * @link     http://shupp.org/Net_Vpopmaild NetVpopmaild
  */
 class Net_Vpopmaild_Exception extends PEAR_Exception
 {
     /**
      * __construct
      *
+     * @param mixed $message message or -ERR message from vpopmaild
+     * @param int   $code    An error code
+     *
      * @access public
-     * @param  mixed $message        message or -ERR message from vpopmaild
-     * @param  int   $code           An error code
      * @return void
      */
     public function __construct($message, $code = 0)
@@ -39,11 +46,11 @@ class Net_Vpopmaild_Exception extends PEAR_Exception
 
         if (preg_match('/^[-]ERR /', $message)) {
             $code = preg_replace('/^-ERR ([^ ]+) .*$/', '\1', $message);
-            $msg = preg_replace('/^-ERR ([^ ]+) (.*$)/', '\2', $message);
+            $msg  = preg_replace('/^-ERR ([^ ]+) (.*$)/', '\2', $message);
         }
 
-        $this->message  = $msg;
-        $this->code     = $code;
+        $this->message = $msg;
+        $this->code    = $code;
     }
 }
 
